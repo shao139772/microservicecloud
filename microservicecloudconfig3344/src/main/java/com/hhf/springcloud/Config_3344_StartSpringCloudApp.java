b9564e7e25adc1ff9fa95f06f6f24225a1e50818
miaomiao <1223602751@qq.com>
2018-08-08 11:36:23 +0800

    新增   config   获取github上的配置信息

---
+++ microservicecloudconfig3344/src/main/java/com/hhf/springcloud/Config_3344_StartSpringCloudApp.java
@@ -0,0 +1,18 @@
+package com.hhf.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Created by cw on 2018/8/7.
+ * 配置的服务端
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class Config_3344_StartSpringCloudApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Config_3344_StartSpringCloudApp.class, args);
+    }
+}
